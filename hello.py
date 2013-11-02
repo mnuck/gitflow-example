9a0b708296842dde7e7f5f27b6ca1a6340dcda91
Matthew Nuckolls <matthew.nuckolls@gmail.com>
2013-11-02 09:32:55 -0500

    Adding copyright notice

--- hello.py
+++ hello.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python
+# Copyright Unpublished 2013.  All rights reserved under the copyright laws by 
+# Matthew Nuckolls Design, LLC. No individual, entity, or government agency
+# may reproduce, release or disclose the material, export the material, use
+# the material for manufacture, or authorize the material to be used in any
+# manner by another party without the written permission of Matthew Nuckolls
+# Design, LLC. If this material is furnished to the Government it is protected
+# by DFAR 252.227-7013(B)(3) - Limited Rights
+# or DFAR 252.227-7014(b)(3) - Restricted Rights. 
 
 print "Hello World!"
